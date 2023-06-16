9fb42ff315c30a0cd70308de36c9b5742990f964
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-06-16 10:49:58 +0300

    export hw7

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,35 +1,65 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
-import initialContacts from '../../components/defaultContacts.json';
-import toast from 'react-hot-toast';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  createContactThunk,
+  deleteContactThunk,
+  getContactsThunk,
+} from './operations';
+import { initialState } from './initialState';
+
+const handlePending = state => {
+  state.status = 'pending';
+};
+
+const handleFulfilledGet = (state, action) => {
+  state.status = 'fulfilled';
+  state.contacts = action.payload;
+  state.error = '';
+};
+
+const handleFulfilledCreate = (state, action) => {
+  state.status = 'fulfilled';
+  state.contacts.push(action.payload);
+  state.error = '';
+};
+
+const handleFulfilledDelete = (state, action) => {
+  state.status = 'fulfilled';
+  state.contacts = state.contacts.filter(
+    contact => contact.id !== action.payload.id
+  );
+  state.error = '';
+};
+
+const handleRejected = (state, action) => {
+  state.status = 'rejected';
+  state.error = action.payload;
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: {
-    initContacts: initialContacts,
-  },
-  reducers: {
-    addContact(state, action) {
-      if (
-        state.initContacts.find(contact => contact.name === action.payload.name)
-      ) {
-        toast.error(`${action.payload.name} is already in contacts.`);
-        return;
-      }
-
-      state.initContacts.push({
-        id: nanoid(),
-        name: action.payload.name,
-        number: action.payload.number,
-      });
-    },
-    deleteContact(state, action) {
-      state.initContacts = state.initContacts.filter(
-        contact => contact.id !== action.payload.id
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(getContactsThunk.fulfilled, handleFulfilledGet)
+      .addCase(createContactThunk.fulfilled, handleFulfilledCreate)
+      .addCase(deleteContactThunk.fulfilled, handleFulfilledDelete)
+      .addMatcher(
+        isAnyOf(
+          getContactsThunk.pending,
+          createContactThunk.pending,
+          deleteContactThunk.pending
+        ),
+        handlePending
+      )
+      .addMatcher(
+        isAnyOf(
+          getContactsThunk.rejected,
+          createContactThunk.rejected,
+          deleteContactThunk.rejected
+        ),
+        handleRejected
       );
-    },
   },
 });
-
-export const { addContact, deleteContact, findContact } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
