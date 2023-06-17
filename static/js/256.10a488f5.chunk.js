6872fa6c952f1dcc6ed193ffe855818471b78f2c
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2023-06-17 11:07:51 +0000

    Deploying to gh-pages from @ Yaroslav-Marinich/goit-react-hw-08-phonebook@1bd9a5d0bdc574758b9d10c714bde790106ffb24 🚀

---
+++ static/js/256.10a488f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[256],{9256:function(e,r,a){a.r(r);var o=a(1087),n=a(184);r.default=function(){return(0,n.jsxs)("div",{className:"errorPageContainer",children:[(0,n.jsx)("h1",{className:"errorPageTitle",children:"Page not found"}),(0,n.jsx)("p",{className:"errorPageText",children:"404 Error"}),(0,n.jsx)(o.rU,{to:"/",className:"errorPageBtn",children:"Back to HomePage"})]})}}}]);
+//# sourceMappingURL=256.10a488f5.chunk.js.map
